b447aff9254eeee0d6a7fa0013994b7845179624
Injo Baik <baikinjo.28@gmail.com>
2017-10-04 17:30:47 -0700

    add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -51,4 +51,45 @@ describe('TodoAPI', () => {
             expect(actualTodos).toEqual(todos);
         });
     });
+
+    describe('filterTodos', () => {
+        var todos = [{
+            id: 1,
+            text: ' filter test',
+            completed: true
+        },{
+            id: 2,
+            text: ' filter test number two',
+            completed: false
+        },{
+            id: 3,
+            text: 'test',
+            completed: true
+        }];
+
+        it('should return all items if showCompleted is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should return all items if showCompleted is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by search text', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'filter');
+            expect(filteredTodos.length).toBe(2);
+        });
+        
+        it('should return all todos if search text is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+    });
 });
