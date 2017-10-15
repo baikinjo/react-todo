f35dbcb522875081f7df341472e563e601621f6b
Injo Baik <baikinjo.28@gmail.com>
2017-10-14 23:27:19 -0700

    update todos location to include uid

--- app/app.jsx
+++ app/app.jsx
@@ -11,6 +11,7 @@ import router from 'app/router/';
 firebase.auth().onAuthStateChanged((user) => {
   if(user){
     store.dispatch(actions.login(user.uid));
+    store.dispatch(actions.startAddTodos());
     hashHistory.push('/todos');
   }else{
     store.dispatch(actions.logout());
@@ -18,9 +19,7 @@ firebase.auth().onAuthStateChanged((user) => {
   }
 });
 
-store.dispatch(actions.startAddTodos());
-
-// Load foundation
+  // Load foundation
 $(document).foundation();
 
 // App css
